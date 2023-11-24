4ea6978a672bb3e86860b6d4e7eae276c240b95d
sk66641 <sanukumar66641@gmail.com>
2023-11-24 19:01:10 +0530

    update

---
+++ Python/practice27.py
@@ -0,0 +1,11 @@
+x=(1,2,3,4,5,6,7,8,9,10)
+y=(3,4)
+print(x+y)
+# lis=list(x)
+# lis.append(5)
+# print(lis)
+strng='sanukumar'
+print(strng[5])
+print((len(strng)))
+print((strng[1:4]))
+print(strng.count('u'))
